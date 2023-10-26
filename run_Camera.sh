cfe7f2dbab2023a4fbf7fe44d941b0a2b169b5d8
duyvu1110 <100179213+duyvu1110@users.noreply.github.com>
2023-10-26 19:33:32 +0700

    /

--- run_Camera.sh
+++ run_Camera.sh
@@ -6,7 +6,7 @@ for layer in 3
 do
     for seed in 2
     do
-        CUDA_VISIBLE_DEVICES=6 python main_SPN.py  \
+        python main_SPN.py  \
         --model_name=Camera_SPN \
         --num_generated=60 \
         --decoder_lr=0.00004 \
@@ -15,7 +15,7 @@ do
         --batch_size=4 \
         --na_rel_coef=0.2 \
         --num_decoder_layers $layer \
-        --max_epoch=10 \
+        --max_epoch=5 \
         --max_grad_norm=10 \
         --random_seed $seed \
         --weight_decay=0.000001 \
@@ -33,13 +33,13 @@ for layer in 3
 do
     for seed in 2
     do
-        CUDA_VISIBLE_DEVICES=3 python main_SPN.py  \
+        python main_SPN.py \
         --model_name=Camera_SPN_test_Amsgrad \
         --num_generated=60 \
         --decoder_lr=0.00004 \
         --encoder_lr=0.00002 \
-        --data_path=data/Camera-COQE \S
-        --batch_size=4 \
+        --data_path=data/Camera-COQE \
+        --batch_size=4\
         --na_rel_coef=0.2 \
         --num_decoder_layers $layer \
         --max_epoch=5 \
