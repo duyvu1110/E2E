4fa93d974351818981352880de1dec9f569cd02a
UwU-tao <hieungo1410@gmail.com>
2023-10-27 09:15:40 +0700

    tuning for vietnamese data

--- models/matcher.py
+++ models/matcher.py
@@ -67,7 +67,7 @@ class HungarianMatcher(nn.Module):
                 - (pred_aspect_start[:, gold_aspect_start] + pred_aspect_end[:, gold_aspect_end]) \
                 - (pred_opinion_start[:, gold_opinion_start] + pred_opinion_end[:, gold_opinion_end]) 
         elif self.matcher == "min":
-            cost = torch.cat([pred_head_start[:, gold_head_start].unsqueeze(1), pred_rel[:, gold_rel].unsqueeze(1), pred_head_end[:, gold_head_end].unsqueeze(
+            cost = torch.cat([pred_sub_start[:, gold_sub_start].unsqueeze(1), pred_rel[:, gold_rel].unsqueeze(1), pred_sub_end[:, gold_sub_end].unsqueeze(
                 1), pred_tail_start[:, gold_tail_start].unsqueeze(1), pred_tail_end[:, gold_tail_end].unsqueeze(1)], dim=1)
             cost = - torch.min(cost, dim=1)[0]
         else:
