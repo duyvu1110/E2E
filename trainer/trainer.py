a5fdd0bef77734bee5dc3e164ef74bdac0851745
UwU-tao <hieungo1410@gmail.com>
2024-01-26 02:00:34 +0700

    ffix

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -155,9 +155,9 @@ class Trainer(nn.Module):
             res = '['
             for index in range(start_index, end_index):
                 if index != end_index - 1:
-                    res += f'"{index}&&{text.split()[index-start_index]}", '
+                    res += f'"{index}&&{text.split(" ")[index-start_index]}", '
                 else:
-                    res += f'"{index}&&{text.split()[index-start_index]}"]'
+                    res += f'"{index}&&{text.split(" ")[index-start_index]}"]'
             return res
 
         whole_input_ids = []
